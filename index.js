7a7c6eaf76c43f3a4e6564df4cf58c826495c609
NazarBaloch04 <153612842+NazarBaloch04@users.noreply.github.com>
2024-03-28 13:24:26 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+import inquire from "inquirer";
+const answer = await inquire.prompt([
+    { message: "enter first number", type: "number", name: "firstNumber" },
+    { message: "enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select any one the operator to perform any operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division", "Exponent", "Modulus"],
+    },
+]);
+// Conditions
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else if (answer.operator === "Exponent") {
+    console.log(Math.pow(answer.firstNumber, answer.secondNumber));
+}
+else if (answer.operator === "Modulus") {
+    console.log(answer.firstNumber % answer.secondNumber);
+}
+else {
+    console.log("Select an invalid operator");
+}
